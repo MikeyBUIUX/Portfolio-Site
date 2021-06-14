a06a479d071993196b3c1917d6ad0eb3e9dbd70b
MikeyBUIUX <82840346+MikeyBUIUX@users.noreply.github.com>
2021-06-14 17:23:08 -0400

    Update 6-14-21
    Added linters for HTML, CSS & Javascript
    Added stylelint.config.js to root
    Cleaned up/beautified HTML
    Added TeaTotaler Case Study page
    Update index.html with new case study
    Updated linked resume
    Added graphics for TeaTotaler

--- Portfolio Site/js/scripts.js
+++ Portfolio Site/js/scripts.js
@@ -1,10 +1,10 @@
-console.log("Welcome to my website!")
+console.log('Welcome to my website!')
 
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
